dec09cc9ce5488d1ba454c09951cc740141ac2c8
Aleksey Belchenkov <u608110@gmail.com>
2019-02-07 06:34:54 +0300

    Create New Project

--- src/main.js
+++ src/main.js
@@ -6,16 +6,11 @@ import Vuetify from 'vuetify'
 import 'vuetify/dist/vuetify.min.css'
 import firebase from 'firebase'
 import firebaseConfig from './config/firebase'
-import VuetifyConfirm from 'vuetify-confirm';
+//import VuetifyConfirm from 'vuetify-confirm';
 
 Vue.use(Vuetify);
 Vue.config.productionTip = false;
 firebase.initializeApp(firebaseConfig);
-
-Vue.use(VuetifyConfirm, {
-  buttonTrueText: 'Да',
-  buttonFalseText: 'Нет'
-});
 
 new Vue({
   router,
